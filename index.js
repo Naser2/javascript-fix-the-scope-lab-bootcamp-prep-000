b052331c9c198a78a11b717f22506e63dc846b25
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 05:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var funkyFunction = function() {
   }
 
 }
-// funkyFunction()()   works in replit
+// funkyFunction()()   works in replit 
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
